1d21ff7529591440d09a084fb12046ec113f8296
ABHIJITH SURESH <abhisuresh2020@gmail.com>
2024-04-22 20:54:42 +0530

    SeconD Commit init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot name="home">Home</x-slot>
+    <h1>Home</h1>
+    <p>Welcome to our website!</p>
+    <p>Here you can find information about our products and services.</p>
+    <p>Feel free to contact us if you have any questions.</p>
+    <p>Thank you for visiting our website!</p>
+</x-layout>
