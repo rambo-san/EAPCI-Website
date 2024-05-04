a1c84f9ab0501e009493139ee67d7b8390559e58
ABHIJITH SURESH <abhisuresh2020@gmail.com>
2024-05-04 10:33:40 +0530

    Courses edit

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -1,27 +1,40 @@
 <x-layout>
   @section('title', 'EAPCI-Courses')
   {{-- mainstart --}}
-  <div class="w-full relative bg-cover h-max ">
-    <div class=" bg-bgwhite">
-      <div class=" text-center h-2/3 right-2">
-        <h1 class="text-4xl md:text-6xl font-bold text-black pt-20  w-full right-2 font-serif">Courses</h1>
-          <div class="flex justify-left">
-            <div class=" md:w-1/2"><img src="{{asset('images/svg/coursebanner.svg')}}" alt="" class=" self-center md:left-20 py-10"></div>
-          </div>
+  
+
+<div id="default-carousel" class="relative w-full h-74" data-carousel="slide">
+  <!-- Carousel wrapper -->
+  <div class="relative w-full h-96 lg:h-2/3 overflow-hidden rounded-b-lg">
+    <!-- Item 1 -->
+    <div class="hidden duration-200 ease-linear" data-carousel-item>
+        <img src="{{asset('images/coursecarousal/caro1.jpg')}}" class="absolute inset-0 w-full h-full object-cover " alt="...">
     </div>
-          <div class="relative -mt-44">
-            <img src="{{asset('images/svg/cwave.svg')}}" alt="" class="w-full">
-            <div class="absolute inset-0 flex flex-col justify-center items-center font-semibold italic text-darkblue"> 
-                <h1 class="text-xl sm:text-2xl md:text-4xl text-center">“Confidentiality is Guaranteed”</h1>
-            </div>
-        
-        <img src="{{asset('images/svg/bannerend.svg')}}" alt="" class="w-full">
-        <div class="relative">
-            
-        </div>
-        
+    <!-- Item 2 -->
+    <div class="hidden duration-200 ease-linear" data-carousel-item>
+        <img src="{{asset('images/coursecarousal/caro2.jpg')}}" class="absolute inset-0 w-full h-full object-cover " alt="...">
+       
       </div>
-    
+</div>
+
+      <!-- Slider controls -->
+      <button type="button" class="absolute top-0 start-0 pt-32 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+          <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+              </svg>
+              <span class="sr-only">Previous</span>
+          </span>
+      </button>
+      <button type="button" class="absolute top-0 end-0 pt-32 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+          <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+              </svg>
+              <span class="sr-only">Next</span>
+          </span>
+      </button>
+  </div>
   
 </div>
   {{-- mainend --}}
