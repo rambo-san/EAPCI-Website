d1c510b318b89881b2ab3e943af7a78d02e218d4
ABHIJITH SURESH <abhisuresh2020@gmail.com>
2024-05-13 19:45:48 +0530

    Services

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
   @section('title') {{'EAPCI - About Us'}} @endsection
-  <div class="relative bg-darkblue text-white min-h-screen flex flex-col items-center justify-center font-pop bg-gradient-to-t from-black to-transparent bg-opacity-60">
+  <div class="relative bg-darkblue text-white min-h-screen flex flex-col items-center justify-center font-pop bg-gradient-to-t from-black to-transparent sm:bg-opacity-60">
     <div class="absolute inset-0 opacity-40 bg-cover blur-md sm:blur-0" style="background-image: url('{{asset('images/jpg/aboutbg.jpg')}}')"></div>
-    <div class="container my-16 mx-auto px-10 md:p-20 z-10 sm:backdrop-blur-sm rounded-xl sm:border border-white contrast-150 text-cente drop-shadow-xl">
-      <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold leading-tight mb-4 ">East African Professional Counselling Institute (EAPCI)</h1>
+    <div class="container my-16 mx-auto p-10 md:p-20 z-10 sm:backdrop-blur-sm rounded-xl sm:border border-white contrast-150 text-cente drop-shadow-xl">
+      <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold leading-tight mb-4 text-center">East African Professional Counselling Institute (EAPCI)</h1>
       <p class="text-md sm:text-xl md:text-2xl leading-relaxed mb-8 text-justify">
         EAPCI was established in 2010 by Counseling and Clinical psychologists from Uganda and Kenya after realizing that both countries share similar psychosocial challenges. A unique experiential learning methodology is applied in all courses and has been found to be very effective. The shared vision of founders was realized when they established an institute in Uganda which has been steadily progressing over the past 13 years. EAPCI is a professional counseling institute registered with the Registrar of Companies (number 119844) in Uganda.
       </p>
@@ -13,38 +13,38 @@
 
 
 
-  <div class="relative bg-bgwhite py-16">
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 class="text-3xl font-bold text-center mb-2 font-serif">Our Story and Mission</h2>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
+  <div class="relative bg-bgwhite p-10 md:p20 text-justify font-pop">
+    <div class="container mx-auto">
+      <h2 class="text-3xl  text-center md:text-left p-3 my-5  font-yesava w-full border-b-2 border-black text-black">Our Story and Mission</h2>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
         EAPCI stays at the forefront of holistic mental health services in Uganda and the Great Lakes Region by constantly incorporating new techniques and skills. We've provided psychosocial counseling services to organizations like Communication for Development Foundation Uganda (CDFU), Reproductive Health Uganda (RH Uganda), and World Vision International Uganda. Currently, we support the Ministry of Gender Labour and Social Development's Gender Unit and the SATI child helpline, as well as Kampala Medical Chambers' mental health clinic hospital.
       </p>
     
-      <h3 class="text-2xl font-bold text-gray-700 mb-2 font-serif">Extensive Service Offerings</h3>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
+      <h3 class="text-3xl  text-center md:text-left text-black p-3 my-5  font-yesava w-full border-b-2 border-black">Extensive Service Offerings</h3>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
         EAPCI offers a comprehensive range of services, including mental health training, clinical counseling supervision, and direct support for mental health and psychosocial support caregivers. We've collaborated with the International Conference of the Great Lakes Region (ICGLR) to provide services for mental health professionals from 12 member countries: Angola, Burundi, Central African Republic, Democratic Republic of Congo, Congo Brazzaville, Kenya, Rwanda, Sudan, South Sudan, Tanzania, Uganda, and Zambia.
       </p>
     
-      <h3 class="text-2xl font-bold text-gray-700 mb-2 font-serif">Our Impact</h3>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
+      <h3 class="text-3xl  text-center md:text-left text-black p-3 my-5  font-yesava w-full border-b-2 border-black">Our Impact</h3>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
         Our work with various organizations has encompassed a wide range of areas. We've helped with employee assistance programs (including psychoeducation, SGBV counseling, stress management, and financial literacy), training and development, performance management, HIV/AIDS intervention, and family mediation.
       </p>
     
-      <h3 class="text-2xl font-bold text-gray-700 mb-2 font-serif">Vision and Mission</h3>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
+      <h3 class="text-3xl  text-center md:text-left text-black p-3 my-5  font-yesava w-full border-b-2 border-black">Vision and Mission</h3>  <p class="text-gray-700 text-lg leading-relaxed mb-8">
         EAPCI's vision is the holistic empowerment of both men and women. We fulfill this mission by providing counseling, training, and community outreach services to individuals and families, promoting healthy relationships.
       </p>
     </div>
     
   </div>
 
-  <div class="bg-gray-100 py-16">
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
-      <div class="border border-gray-200 rounded-lg p-4 shadow-md">
-        <h3 class="text-xl font-bold text-gray-800 mb-4">EAPCI Niche</h3>
-        <p class="text-gray-700 text-base leading-relaxed mb-4">
+  <div class="bg-darkblue p-10 md:p-20 bg-gradient-to-t from-darkblue to-bgwhite text-justify">
+    <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div class=" rounded-lg p-5 shadow-2xl text-bgwhite bg-darkblue hover:shadow-[3px_3px_7px_#2C6173] transition duration-300 ease-in-out">
+        <h3 class="text-2xl font-bold mb-4">EAPCI Niche</h3>
+        <p class="text-base leading-relaxed mb-4 italic">
           EAPCI brings expertise in mental health and psychosocial support, relevant in the East African Region, the Great Lakes Region, and across many sectors in Government, Business Organizations, and Higher institutions of education to assist in analysis, benchmarking, and problem-solving. EAPCI consultants are well-grounded in theory and practice in their areas of expertise and armed with confidentiality, i.e. they approach a project from both systems and reality perspective. EAPCI understands schedules, budgets, and timelines, and is committed to completing projects on time and within the budget framework. Their services have been made available to their clients in a confidential environment and available 24/7 and accessible by telephone, e-mail, and other electronic means.
         </p>
       </div>
-        <div class="bg-indigo-50 border border-gray-200 rounded-lg p-8 shadow-md">
-          <h3 class="text-2xl font-bold text-gray-800 mb-4">Core Values</h3>
-          <ul class="list-disc text-gray-700 ml-4">
+        <div class="bg-bgwhite border border-gray-200 rounded-lg p-8 shadow-2xl hover:shadow-[3px_3px_7px_#2C6173] transition duration-300 ease-in-out">
+          <h3 class="text-2xl font-bold text-black mb-4">Core Values</h3>
+          <ul class="list-disc text-black ml-4 italic">
             <li>Confidentiality</li>
             <li>Empathy</li>
             <li>Genuineness</li>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -117,7 +117,6 @@
                 });
 
 
-
             </script>
         {{-- End Script for scroll --}}
     <!--Body End-->
