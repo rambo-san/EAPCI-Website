32752350bbb08688a3151cb70812a234279b25bc
ABHIJITH SURESH <abhisuresh2020@gmail.com>
2024-05-20 13:12:24 +0530

    JS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -102,6 +102,29 @@
                 window.addEventListener('load', function(){ 
                     preloader.style.display = 'none';
                 });
+
+                window.onscroll = function() {scrollFunction()}; // call scrollFunction when user scrolls
+
+                function scrollFunction() {
+                    var button = document.getElementById('moveToTopButton');
+                    var nav = document.getElementById('navbar');
+                    if (window.scrollY > 20) {
+                        button.classList.remove('hidden');
+                        nav.classList.add('drop-shadow-xl');
+                    } else {
+                        button.classList.add('hidden');
+                        nav.classList.remove('drop-shadow-xl');
+                    }
+                }
+
+                function scrollToTop() {
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                }
+                //preloader
+
+                setInterval(() => {
+                    document.querySelector('[data-carousel-next]').click();
+                }, 4000);
         </script>
     <!--Body End-->
 
